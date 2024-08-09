2ee2cef33808a7007492dff23bf5d34b4cedce81
shubham88fru <shubham88fru@yahoo.com>
2024-08-08 21:22:35 -0500

    access modifiers

--- app.ts
+++ app.ts
@@ -1,1 +1,25 @@
-// class
+class Department {
+  public name: string;
+  private employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe() {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("Technology");
+accounting.describe();
+console.log(accounting);
