2edd622c98dd520bd91bc4fd9374ec3db076ae59
shubham88fru <shubham88fru@yahoo.com>
2024-08-11 21:43:26 -0500

    interfaces

--- app.ts
+++ app.ts
@@ -23,3 +23,31 @@ class Department {
 const accounting = new Department("Technology");
 accounting.describe();
 console.log(accounting);
+
+//interface.
+interface Person {
+  name: string;
+  age: number;
+  sex?: string; //optional property.
+
+  greet(phrase: string): void;
+}
+
+let user1: Person;
+user1 = {
+  name: "Max",
+  age: 30,
+  greet(phrase: string) {
+    console.log("Hi");
+  },
+};
+
+//interface as a function type.
+interface AddFn {
+  (a: number, b: number): number; //function signature.
+}
+
+let add: AddFn;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
