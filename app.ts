98b3e7f742d9915da8a6dd889f6a8eff65d17785
shubham88fru <shubham88fru@yahoo.com>
2024-08-22 21:16:34 -0700

    generics

---
+++ app.ts
@@ -0,0 +1,77 @@
+const names = ["Max", "Manuel"];
+const names_generic: Array<string> = [];
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
+});
+
+promise.then((data) => {
+  data.split("");
+});
+
+//generics and constraints with functions.
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+const mergeObj2 = merge({ fname: "Shubham" }, { lname: "Singh" });
+
+//
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length > 0) {
+    descriptionText = `Got ${element.length} element(s)`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe([1, 2]));
+
+//
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  //ensure that key must be a key from T.
+  return `Value: ${obj[key]}`;
+}
+console.log(extractAndConvert({ name: "Max" }, "name"));
+
+//
+class DataStorage<T> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+numberStorage.removeItem(1);
+
+const objStorage = new DataStorage<object>();
+objStorage.addItem({ name: "Max" });
+objStorage.addItem({ name: "Manu" });
+objStorage.removeItem({ name: "Manu" });
