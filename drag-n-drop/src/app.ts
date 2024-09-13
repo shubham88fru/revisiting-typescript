13456044c842279bc371c826b745422596925abb
shubham88fru <shubham88fru@yahoo.com>
2024-09-13 16:09:10 -0700

    adding active and finished projects section

--- drag-n-drop/src/app.ts
+++ drag-n-drop/src/app.ts
@@ -64,6 +64,36 @@ function autobind(
   return adjustedDescriptor;
 }
 
+class ProjectList {
+  templateEl: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateEl = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -162,3 +192,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
