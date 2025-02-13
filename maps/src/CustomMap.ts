4aa3e726488c35925f2461f62a9a50b13a30edb9
shubham88fru <shubham88fru@yahoo.com>
2025-02-13 15:29:27 -0700

    maps project

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,31 @@
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(div: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(div)!, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
---
+++ maps/src/index.ts
@@ -0,0 +1,5 @@
+//<reference path="@types/google.maps" />
+
+import { CustomMap } from "./CustomMap";
+
+const map = new CustomMap("map");
