45131f06594be0eb8efafc7d0cc6ca54e3687f6d
shubham88fru <shubham88fru@yahoo.com>
2025-02-19 13:51:50 -0700

    use generics to parameterize the csv reader class.

--- stats/src/CSVFileReader.ts
+++ stats/src/CSVFileReader.ts
@@ -1,13 +1,11 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
 
-//tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+export abstract class CSVFileReader<T> {
+  data: T[] = [];
 
-export class CSVFileReader {
-  data: MatchData[] = [];
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T;
+
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
@@ -17,16 +15,6 @@ export class CSVFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult, //type assertion
-          row[6],
-        ];
-      });
+      .map(this.mapRow);
   }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,7 +1,7 @@
-import { CSVFileReader } from "./CSVFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new CSVFileReader("football.csv");
+const reader = new MatchReader("football.csv");
 reader.read();
 
 let manUnitedWins = 0;
